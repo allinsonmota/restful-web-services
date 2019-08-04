5f63db1a1da7e6b219c067626388d7647d055d58
Allinson Mota <allinsonmota@gmail.com>
2019-08-04 17:39:39 -0300

    Add static and dynamic filtering

---
+++ src/main/java/com/allinsonmota/rest/webservices/restfullwebservices/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.allinsonmota.rest.webservices.restfullwebservices;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Allinson Mota",
+            "https://github.com/allinsonmota",
+            "allinsonmota@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Awesome API title",
+            "Awesome API description",
+            "1.0",
+            "urn:tos",
+            DEFAULT_CONTACT,
+            "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0");
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
+    @Bean
+    public Docket aip() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+                ;
+    }
+}
---
+++ src/main/java/com/allinsonmota/rest/webservices/restfullwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.allinsonmota.rest.webservices.restfullwebservices.exception;
+
+import com.allinsonmota.rest.webservices.restfullwebservices.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.List;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUseNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        StringBuilder message = new StringBuilder();
+        for (ObjectError error :allErrors) {
+            message.append(" ").append(error.getDefaultMessage());
+        }
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), "Validation Failed", message.toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
