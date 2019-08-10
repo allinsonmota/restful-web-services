32ff6348784409c17feb3f240709e226afae5f4b
Allinson Mota <allinsonmota@gmail.com>
2019-08-10 15:34:22 -0300

    Add versioning

---
+++ src/main/java/com/allinsonmota/rest/webservices/restfullwebservices/versioning/PersonV2.java
@@ -0,0 +1,21 @@
+package com.allinsonmota.rest.webservices.restfullwebservices.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2() {
+    }
+
+    public PersonV2(Name name) {
+        super();
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}
